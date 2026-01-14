5b4de35a605d5df8081993f2dffa4359cff305bb
Pruthvi-dhani <87530402+Pruthvi-dhani@users.noreply.github.com>
2026-01-14 22:23:17 +0000

    examples to create flux from stream

--- src/main/java/com/sec03/FluxJustDemo.java
+++ src/main/java/com/sec03/FluxJustDemo.java
@@ -18,5 +18,22 @@ public class FluxJustDemo {
 
         var list = List.of(1, 2, 3, 4);
         Flux.fromIterable(list).subscribe(i -> System.out.println(i));
+
+        // stream can be consumed only once
+        var simpleList = List.of(1, 2, 3, 4, 5);
+        var simpleStream = simpleList.stream();
+        var flux = Flux.fromStream(simpleStream);
+        flux.subscribe(i -> System.out.println("Sub 1 received from stream: " + i),
+                err -> System.out.println("Sub 1 received error: " + err.getMessage()));
+        flux.subscribe(i -> System.out.println("Sub 2 received from stream: " + i),
+                err -> System.out.println("Sub 2 received error: " + err.getMessage()));
+
+        // stream can be consumed multiple times as we re-create the stream every time
+        var nexList = List.of(2, 3, 5);
+        var nexFlux = Flux.fromStream(nexList::stream);
+        nexFlux.subscribe(i -> System.out.println("Nex Sub 1 received from stream: " + i),
+                err -> System.out.println("Nex Sub 1 received error: " + err.getMessage()));
+        nexFlux.subscribe(i -> System.out.println("Nex Sub 2 received from stream: " + i),
+                err -> System.out.println("Nex Sub 2 received error: " + err.getMessage()));
     }
 }
